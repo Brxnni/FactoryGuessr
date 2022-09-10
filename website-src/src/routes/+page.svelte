c0a89286888e3028ab121a9d4893e12c6efe4624
Brxnni <arne.norvin@gmail.com>
2022-09-10 17:05:43 +0200

    Update image paths according to ./static changes

--- website-src/src/routes/+page.svelte
+++ website-src/src/routes/+page.svelte
@@ -35,8 +35,8 @@
 		<LinedHeading type="h3" text="Contact"/>
 
 		<div>
-			<a href = "https://github.com/Brxnni" tabindex="0"><ImageBlock text="GitHub Repo" altText="GitHub Logo" imageSrc="./github.svg"></ImageBlock></a>
-			<span tabindex="0"><ImageBlock text="Discord: Brxnni#0553" altText="Discord Logo" imageSrc="./discord.svg"></ImageBlock></span>
+			<a href = "https://github.com/Brxnni" tabindex="0"><ImageBlock text="GitHub Repo" altText="GitHub Logo" imageSrc="./img/github.svg"></ImageBlock></a>
+			<span tabindex="0"><ImageBlock text="Discord: Brxnni#0553" altText="Discord Logo" imageSrc="./img/discord.svg"></ImageBlock></span>
 		</div>
 
 		<LinedHeading type="h3" text="Credits"/>
